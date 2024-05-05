d1b8a9558aaf9449a52f8ae415d5cf30ca1e0a84
Kirill Skofenko <tor505274@gmail.com>
2024-05-05 20:12:27 +0300

    fix Flask decorators export

--- ui-config-generator-flask/flaskUIGenerator/__init__.py
+++ ui-config-generator-flask/flaskUIGenerator/__init__.py
@@ -1,3 +1,7 @@
 from .main import FlaskUIGenerator
 from .decorators import display_name
 from .decorators import ui_field
+from .decorators import response_body
+from .decorators import request_body
+from .decorators import path_param
+from .decorators import query_param
